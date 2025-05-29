5f979132e98803b760c33aa56ee35f2c3afe6af3
alexleedfe <alex.lee@education.gov.uk>
2025-05-29 15:40:42 +0100

    adding utm example
    adding utm example

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,4 +5,22 @@
 
 window.GOVUKPrototypeKit.documentReady(() => {
   // Add JavaScript here
+
+function getUTMParameters() {
+  const params = new URLSearchParams(window.location.search);
+  return {
+    utm_name: params.get('utm_name'),
+  };
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const utms = getUTMParameters();
+
+  if (utms.utm_name) {
+    document.getElementById("name").textContent = `${utms.utm_name}`;
+  }
+ 
+});
+
+
 })
