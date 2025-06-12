477243193521377d6a2a1916a8f8f86a2297a1d9
alexleedfe <alex.lee@education.gov.uk>
2025-06-12 11:31:48 +0100

    adding functionality for passing name
    adding functionality for passing name

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,22 +14,58 @@ function getUTMParameters() {
   };
 }
 
+
 document.addEventListener("DOMContentLoaded", () => {
 
   const utms = getUTMParameters();
 
   if (utms.utm_name) {
-    document.getElementById("name").textContent = `${utms.utm_name}`;
-    document.getElementById("name2").textContent = `${utms.utm_name}`;
-    document.getElementById("testname").textContent = `${utms.utm_test}`;
+    const nameEl = document.getElementById("name");
+    const name2El = document.getElementById("name2");
+
+    if (nameEl) {
+      nameEl.textContent = utms.utm_name;
+    }
+
+    if (name2El) {
+      name2El.textContent = utms.utm_name;
+    }
   }
 
   if (utms.utm_test) {
-    document.getElementById("testname").value = `${utms.utm_test}`;
+    const testNameEl = document.getElementById("testname");
+
+    if (testNameEl) {
+      testNameEl.textContent = utms.utm_test;
+    }
+  }
+
+  //email link 
+
+  const emailSubmit = document.getElementById("emailsubmit");
+
+  if (emailSubmit) {
+    if (utms.utm_name) {
+      const link = document.getElementById("emailsubmit");
+      const originalHref = link.getAttribute("href");
+      const separator = originalHref.includes('?') ? '&' : '?';
+      const newHref = `${originalHref}${separator}utm_name=${encodeURIComponent(utms.utm_name)}`;
+      link.setAttribute("href", newHref);
+    }
+  }
+
+  //form submit
+
+  const formSubmit = document.getElementById("submit");
+
+  if (formSubmit) {
+    document.getElementById("therename").value = utms.utm_name
+    document.getElementById("testname").value = utms.utm_test
     document.getElementById('submit').submit();
   }
+  
  
 });
 
 
-})
+});
