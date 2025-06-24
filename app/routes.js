6c79fed614addb99ef9ec99c00d6ddba815f408c
alexleedfe <alex.lee@education.gov.uk>
2025-06-24 12:14:51 +0100

    adding functionality for routing notifications with flash, and cookies page

--- app/routes.js
+++ app/routes.js
@@ -5,5 +5,19 @@
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
+const flash = require('connect-flash')
+router.use(flash())
+
+router.all('*', (req, res, next) => {
+  res.locals.flash = req.flash('success') // pass through 'success' messages only
+  next()
+})
+
 
 // Add your routes here
+
+
+router.post('/footer/cookies', (req, res) => {
+    req.flash('success', 'Cookies preferences saved')
+    res.redirect('/footer/cookies')
+  })
