3668e73335c2521dc810d6e68f54a62613af541d
alexleedfe <alex.lee@education.gov.uk>
2025-10-20 14:29:36 +0100

    test share button code

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -222,3 +222,23 @@ if (browserId) {
   const el = document.getElementById(browserId);
   if (el) el.style.display = "block";
 }
+
+
+const shareData = {
+  title: "MDN",
+  text: "Learn web development on MDN!",
+  url: "https://developer.mozilla.org",
+};
+
+const btn = document.querySelector("button");
+const resultPara = document.querySelector(".result");
+
+// Share must be triggered by "user activation"
+btn.addEventListener("click", async () => {
+  try {
+    await navigator.share(shareData);
+    resultPara.textContent = "MDN shared successfully";
+  } catch (err) {
+    resultPara.textContent = `Error: ${err}`;
+  }
+});
