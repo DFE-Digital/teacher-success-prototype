be378db22b8f75e8e371bbad6bd62c2beedeebc6
alexleedfe <alex.lee@education.gov.uk>
2025-10-20 15:04:16 +0100

    hovering bubble concept home page

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -224,21 +224,3 @@ if (browserId) {
 }
 
 
-const shareData = {
-  title: "MDN",
-  text: "Learn web development on MDN!",
-  url: "https://developer.mozilla.org",
-};
-
-const btn = document.querySelector("button");
-const resultPara = document.querySelector(".result");
-
-// Share must be triggered by "user activation"
-btn.addEventListener("click", async () => {
-  try {
-    await navigator.share(shareData);
-    resultPara.textContent = "MDN shared successfully";
-  } catch (err) {
-    resultPara.textContent = `Error: ${err}`;
-  }
-});
